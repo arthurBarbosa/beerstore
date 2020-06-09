f16268429b34e401d09cdeab33250edaa657a69e
Arthur Barbosa <arthurbarbosa@MacBook-Pro-de-Arthur.local>
2020-06-08 22:02:03 -0400

    [added] status code

--- src/main/java/com/arthurbarbosa/beerstore/resource/BeerResource.java
+++ src/main/java/com/arthurbarbosa/beerstore/resource/BeerResource.java
@@ -3,13 +3,18 @@ package com.arthurbarbosa.beerstore.resource;
 import com.arthurbarbosa.beerstore.model.Beer;
 import com.arthurbarbosa.beerstore.repository.Beers;
 import com.arthurbarbosa.beerstore.service.BeerService;
+import com.arthurbarbosa.beerstore.service.exception.BeerNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/beers")
@@ -26,16 +31,23 @@ public class BeerResource {
         return beers.findAll();
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
+    @GetMapping("/{id}")
+    public ResponseEntity<Beer> findById(@PathVariable Long id) {
+        Optional<Beer> obj = beers.findById(id);
+        return ResponseEntity.ok().body(obj.orElseThrow(() -> new BeerNotFoundException()));
+    }
+
     @PostMapping
-    public Beer create(@Valid @RequestBody Beer beer) {
-        return beerService.save(beer);
+    public ResponseEntity<Beer> create(@Valid @RequestBody Beer beer) {
+        Beer obj = beerService.save(beer);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 
     @PutMapping(value = "/{id}")
-    public Beer update(@Valid @PathVariable Long id, @RequestBody Beer beer) {
+    public ResponseEntity<Beer> update(@Valid @PathVariable Long id, @RequestBody Beer beer) {
         beer.setId(id);
-        return beerService.save(beer);
+        return ResponseEntity.ok().body(beerService.save(beer));
     }
 
     @DeleteMapping("/{id}")
